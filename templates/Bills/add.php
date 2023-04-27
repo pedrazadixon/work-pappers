b5072ea11fb0f0015588363a2795711c05f8750c
Dixon Pedraza <pedrazadixon@gmail.com>
2023-04-26 22:27:40 -0500

    themify bills

--- templates/Bills/add.php
+++ templates/Bills/add.php
@@ -1,35 +1,67 @@
-<?php
-/**
- * @var \App\View\AppView $this
- * @var \App\Model\Entity\Bill $bill
- * @var \Cake\Collection\CollectionInterface|string[] $quotes
- * @var \Cake\Collection\CollectionInterface|string[] $suppliers
- * @var \Cake\Collection\CollectionInterface|string[] $bankAccounts
- * @var \Cake\Collection\CollectionInterface|string[] $billsStatuses
- */
-?>
 <div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('List Bills'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="bills form content">
-            <?= $this->Form->create($bill) ?>
-            <fieldset>
-                <legend><?= __('Add Bill') ?></legend>
-                <?php
-                    echo $this->Form->control('quote_id', ['options' => $quotes, 'empty' => true]);
-                    echo $this->Form->control('supplier_id', ['options' => $suppliers, 'empty' => true]);
-                    echo $this->Form->control('bank_account_id', ['options' => $bankAccounts, 'empty' => true]);
-                    echo $this->Form->control('status_id', ['options' => $billsStatuses, 'empty' => true]);
-                    echo $this->Form->control('date', ['empty' => true]);
-                ?>
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
+    <div class="col-12">
+
+        <div class="card my-4">
+            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                <div class="bg-gradient-secondary shadow-secondary border-radius-lg pt-4 pb-3">
+                    <div class="row mx-3">
+                        <div class="col-12">
+                            <h6 class="text-white text-capitalize">New Bill</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body mt-2">
+
+                <?= $this->Form->create($bill) ?>
+
+                <div class="row">
+
+                    <div class="col-12 col-sm-6">
+                        <div class="input-group input-group-static mb-4">
+                            <label class="ms-0">quote_id</label>
+                            <?= $this->Form->control('quote_id', ['options' => $quotes, 'empty' => true, 'class' => 'form-control', 'label' => false]); ?>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-sm-6">
+                        <div class="input-group input-group-static mb-4">
+                            <label class="ms-0">supplier_id</label>
+                            <?= $this->Form->control('supplier_id', ['options' => $suppliers, 'empty' => true, 'class' => 'form-control', 'label' => false]); ?>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-sm-6">
+                        <div class="input-group input-group-static mb-4">
+                            <label class="ms-0">bank_account_id</label>
+                            <?= $this->Form->control('bank_account_id', ['options' => $bankAccounts, 'empty' => true, 'class' => 'form-control', 'label' => false]); ?>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-sm-6">
+                        <div class="input-group input-group-static mb-4">
+                            <label class="ms-0">status_id</label>
+                            <?= $this->Form->control('status_id', ['options' => $billsStatuses, 'empty' => true, 'class' => 'form-control', 'label' => false]); ?>
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <div class="input-group input-group-static mb-4">
+                            <label>date</label>
+                            <?= $this->Form->control('date', ['class' => 'form-control', 'label' => false]); ?>
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <?= $this->Html->link(__('Cancel'), ['action' => 'index'], ['class' => 'btn btn-danger me-3']) ?>
+                        <?= $this->Form->button(__('Submit'), ['class' => 'btn btn-success me-3']) ?>
+                    </div>
+
+                </div>
+
+                <?= $this->Form->end() ?>
+
+            </div>
         </div>
     </div>
 </div>
