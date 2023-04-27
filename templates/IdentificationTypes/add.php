3032a223fca8498d7b79ac49e1888eedd859d24f
Dixon Pedraza <pedrazadixon@gmail.com>
2023-04-27 11:35:17 -0500

    themify add views

--- templates/IdentificationTypes/add.php
+++ templates/IdentificationTypes/add.php
@@ -1,28 +1,46 @@
-<?php
-/**
- * @var \App\View\AppView $this
- * @var \App\Model\Entity\IdentificationType $identificationType
- */
-?>
 <div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('List Identification Types'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="identificationTypes form content">
-            <?= $this->Form->create($identificationType) ?>
-            <fieldset>
-                <legend><?= __('Add Identification Type') ?></legend>
-                <?php
-                    echo $this->Form->control('name');
-                    echo $this->Form->control('acronym');
-                ?>
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
+    <div class="col-12">
+
+        <div class="card my-4">
+            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                <div class="bg-gradient-secondary shadow-secondary border-radius-lg pt-4 pb-3">
+                    <div class="row mx-3">
+                        <div class="col-12">
+                            <h6 class="text-white text-capitalize">New Identification Type</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body mt-2">
+
+                <?= $this->Form->create($identificationType) ?>
+
+                <div class="row">
+
+                    <div class="col-12 col-sm-6">
+                        <div class="input-group input-group-static mb-4">
+                            <label>name</label>
+                            <?= $this->Form->control('name', ['class' => 'form-control', 'label' => false]); ?>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-sm-6">
+                        <div class="input-group input-group-static mb-4">
+                            <label>acronym</label>
+                            <?= $this->Form->control('acronym', ['class' => 'form-control', 'label' => false]); ?>
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <?= $this->Html->link(__('Cancel'), ['action' => 'index'], ['class' => 'btn btn-danger me-3']) ?>
+                        <?= $this->Form->button(__('Submit'), ['class' => 'btn btn-success me-3']) ?>
+                    </div>
+
+                </div>
+
+                <?= $this->Form->end() ?>
+
+            </div>
         </div>
     </div>
 </div>
--- templates/QuotesStatuses/add.php
+++ templates/QuotesStatuses/add.php
@@ -1,27 +1,39 @@
-<?php
-/**
- * @var \App\View\AppView $this
- * @var \App\Model\Entity\QuotesStatus $quotesStatus
- */
-?>
 <div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('List Quotes Statuses'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="quotesStatuses form content">
-            <?= $this->Form->create($quotesStatus) ?>
-            <fieldset>
-                <legend><?= __('Add Quotes Status') ?></legend>
-                <?php
-                    echo $this->Form->control('name');
-                ?>
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
+    <div class="col-12">
+
+        <div class="card my-4">
+            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                <div class="bg-gradient-secondary shadow-secondary border-radius-lg pt-4 pb-3">
+                    <div class="row mx-3">
+                        <div class="col-12">
+                            <h6 class="text-white text-capitalize">New Quotes Status</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body mt-2">
+
+                <?= $this->Form->create($quotesStatus) ?>
+
+                <div class="row">
+
+                    <div class="col-12 col-sm-6">
+                        <div class="input-group input-group-static mb-4">
+                            <label>name</label>
+                            <?= $this->Form->control('name', ['class' => 'form-control', 'label' => false]); ?>
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <?= $this->Html->link(__('Cancel'), ['action' => 'index'], ['class' => 'btn btn-danger me-3']) ?>
+                        <?= $this->Form->button(__('Submit'), ['class' => 'btn btn-success me-3']) ?>
+                    </div>
+
+                </div>
+
+                <?= $this->Form->end() ?>
+
+            </div>
         </div>
     </div>
 </div>
